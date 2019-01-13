1747e1e40dea7d18509b39ffc40030fcde396dad
Grenze <hit.grenze.plus@gmail.com>
2019-01-13 23:07:04 +0800

    naivetable

--- BaseTable.h
+++ BaseTable.h
@@ -10,24 +10,24 @@
 #include <sstream>
 
 #include "BaseTable.h"
+#include "BitsUtil.h"
 
+//Generally every bucket contains slots with the number of associativity,
+//slot contains key's hash tag and location of key-value pair
 namespace CuckooHash{
-    template <size_t bits_per_tag, size_t associativity>
+    template </*size_t bits_per_tag, */size_t bits_per_slot, size_t associativity>
     class BaseTable {
-        static const size_t kTagsPerBucket = associativity;
-        //plus 7 to make sure there is enough space per Bucket to store tags
-        //when bits_per_tag = 1
-        static const size_t kBytesPerBucket =
-                (bits_per_tag * kTagsPerBucket + 7) >> 3;
-        //type of Tag is pointer which points to the data
-        //static const uint32_t kTagMask = (1ULL << bits_per_tag) - 1;
-        // NOTE: accommodate extra buckets if necessary to avoid overrun
-        // as we always read a uint64, cache friendly
-        static const size_t kPaddingBuckets =
-                ((((kBytesPerBucket + 7) / 8) * 8) - 1) / kBytesPerBucket;
+        static const size_t slotsPerBucket = associativity;
+        //plus 7 to make sure there is enough space per Bucket to store slots
+        //when bits_per_slot = 1
+        static const size_t bytesPerBucket =
+                (bits_per_slot * slotsPerBucket + 7) >> 3;
+        //static const uint32_t tagMask = static_cast<const uint32_t>((1ULL << bits_per_tag) - 1);
+        static const size_t paddingBuckets =
+                ((((bytesPerBucket + 7) / 8) * 8) - 1) / bytesPerBucket;
 
         struct Bucket {
-            char bits_[kBytesPerBucket];
+            char bits_[bytesPerBucket];
         } __attribute__((__packed__));
 
         // using a pointer adds one more indirection
@@ -36,9 +36,9 @@ namespace CuckooHash{
 
     public:
         explicit BaseTable(const size_t num) : num_buckets_(num) {
-            //cout<<kBytesPerBucket<<" "<<kPaddingBuckets<<" "<<num_buckets_<<" "<<kTagMask<<" "<<endl;
-            buckets_ = new Bucket[num_buckets_ + kPaddingBuckets];
-            memset(buckets_, 0, kBytesPerBucket * (num_buckets_ + kPaddingBuckets));
+            //cout<<bytesPerBucket<<" "<<paddingBuckets<<" "<<num_buckets_<<" "<<endl;
+            buckets_ = new Bucket[num_buckets_ + paddingBuckets];
+            memset(buckets_, 0, bytesPerBucket * (num_buckets_ + paddingBuckets));
         }
 
         ~BaseTable() {
@@ -50,94 +50,91 @@ namespace CuckooHash{
         }
 
         /*uint32_t TagMask() const{
-            return kTagMask;
+            return tagMask;
         }*/
 
         size_t SizeInBytes() const {
-            return kBytesPerBucket * num_buckets_;
+            return bytesPerBucket * num_buckets_;
         }
 
-        size_t SizeInTags() const {
-            return kTagsPerBucket * num_buckets_;
+        size_t SizeInSlots() const {
+            return slotsPerBucket * num_buckets_;
         }
 
         std::string Info() const {
             std::stringstream ss;
-            ss << "SingleHashtable with tag size: " << bits_per_tag << " bits \n";
-            ss << "\t\tAssociativity: " << kTagsPerBucket << "\n";
+            //ss << "BaseTable with tag size and slot size: " << bits_per_tag << " / " bits_per_slot << " bits \n";
+            ss << "BaseTable with slot size: " << bits_per_slot << " bits \n";
+            ss << "\t\tAssociativity: " << slotsPerBucket << "\n";
             ss << "\t\tTotal # of rows: " << num_buckets_ << "\n";
-            ss << "\t\tTotal # slots: " << SizeInTags() << "\n";
+            ss << "\t\tTotal # slots: " << SizeInSlots() << "\n";
             return ss.str();
         }
 
-        // read tag from pos(i,j)
-        inline uint64_t ReadTag(const size_t i, const size_t j) const {
+        // read slot from pos(i,j)
+        inline uint64_t ReadSlot(const size_t i, const size_t j) const {
             const char *p = buckets_[i].bits_;
-            uint64_t tag;
+            uint64_t slot;
             /* following code only works for little-endian */
-            if (bits_per_tag == 2) {
-                tag = *((uint8_t *)p) >> (j * 2);
-            } else if (bits_per_tag == 4) {
+            if (bits_per_slot == 2) {
+                slot = *((uint8_t *)p) >> (j * 2);
+            } else if (bits_per_slot == 4) {
                 p += (j >> 1);
-                tag = *((uint8_t *)p) >> ((j & 1) << 2);
-            } else if (bits_per_tag == 8) {
+                slot = *((uint8_t *)p) >> ((j & 1) << 2);
+            } else if (bits_per_slot == 8) {
                 p += j;
-                tag = *((uint8_t *)p);
-            } else if (bits_per_tag == 12) {
+                slot = *((uint8_t *)p);
+            } else if (bits_per_slot == 12) {
                 p += j + (j >> 1);
-                tag = *((uint16_t *)p) >> ((j & 1) << 2);
-            } else if (bits_per_tag == 16) {
+                slot = *((uint16_t *)p) >> ((j & 1) << 2);
+            } else if (bits_per_slot == 16) {
                 p += (j << 1);
-                tag = *((uint16_t *)p);
-            } else if (bits_per_tag == 32) {
-                tag = ((uint32_t *)p)[j];
-            } else if (bits_per_tag == 64) {
-                tag = ((uint64_t *)p)[j];
+                slot = *((uint16_t *)p);
+            } else if (bits_per_slot == 32) {
+                slot = ((uint32_t *)p)[j];
+            } else if (bits_per_slot == 64) {
+                slot = ((uint64_t *)p)[j];
             }
-            //uint32_t raw data shold be masked
-            //return tag & kTagMask;
-            return tag;
+            return slot;
         }
 
-        // write tag to pos(i,j)
-        inline void WriteTag(const size_t i, const size_t j, const uint64_t t) {
+        // write slot to pos(i,j)
+        inline void WriteSlot(const size_t i, const size_t j, const uint64_t s) {
             char *p = buckets_[i].bits_;
-            //cout<<(t==(t&kTagMask)) <<endl;
-            //raw tag had been masked before passed in
-            uint64_t tag = t /*& kTagMask*/;
+            uint64_t slot = s;
             /* following code only works for little-endian */
-            if (bits_per_tag == 2) {
-                *((uint8_t *)p) |= tag << (2 * j);
-            } else if (bits_per_tag == 4) {
+            if (bits_per_slot == 2) {
+                *((uint8_t *)p) |= slot << (2 * j);
+            } else if (bits_per_slot == 4) {
                 p += (j >> 1);
                 if ((j & 1) == 0) {
                     *((uint8_t *)p) &= 0xf0;
-                    *((uint8_t *)p) |= tag;
+                    *((uint8_t *)p) |= slot;
                 } else {
                     *((uint8_t *)p) &= 0x0f;
-                    *((uint8_t *)p) |= (tag << 4);
+                    *((uint8_t *)p) |= (slot << 4);
                 }
-            } else if (bits_per_tag == 8) {
-                ((uint8_t *)p)[j] = tag;
-            } else if (bits_per_tag == 12) {
+            } else if (bits_per_slot == 8) {
+                ((uint8_t *)p)[j] = static_cast<uint8_t>(slot);
+            } else if (bits_per_slot == 12) {
                 p += (j + (j >> 1));
                 if ((j & 1) == 0) {
                     ((uint16_t *)p)[0] &= 0xf000;
-                    ((uint16_t *)p)[0] |= tag;
+                    ((uint16_t *)p)[0] |= slot;
                 } else {
                     ((uint16_t *)p)[0] &= 0x000f;
-                    ((uint16_t *)p)[0] |= (tag << 4);
+                    ((uint16_t *)p)[0] |= (slot << 4);
                 }
-            } else if (bits_per_tag == 16) {
-                ((uint16_t *)p)[j] = tag;
-            } else if (bits_per_tag == 32) {
-                ((uint32_t *)p)[j] = tag;
-            } else if (bits_per_tag == 64){
-                ((uint64_t *)p)[j] = tag;
+            } else if (bits_per_slot == 16) {
+                ((uint16_t *)p)[j] = static_cast<uint16_t>(slot);
+            } else if (bits_per_slot == 32) {
+                ((uint32_t *)p)[j] = static_cast<uint32_t>(slot);
+            } else if (bits_per_slot == 64){
+                ((uint64_t *)p)[j] = slot;
             }
         }
 
-        inline bool FindTagInBuckets(const size_t i1, const size_t i2,
+        inline bool FindSlotInBuckets(const size_t i1, const size_t i2,
                                      const uint64_t tag) const {
             const char *p1 = buckets_[i1].bits_;
             const char *p2 = buckets_[i2].bits_;
@@ -146,16 +143,16 @@ namespace CuckooHash{
             uint64_t v2 = *((uint64_t *)p2);
 
             // caution: unaligned access & assuming little endian
-            if (bits_per_tag == 4 && kTagsPerBucket == 4) {
+            if (bits_per_slot == 4 && slotsPerBucket == 4) {
                 return hasvalue4(v1, tag) || hasvalue4(v2, tag);
-            } else if (bits_per_tag == 8 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 8 && slotsPerBucket == 4) {
                 return hasvalue8(v1, tag) || hasvalue8(v2, tag);
-            } else if (bits_per_tag == 12 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 12 && slotsPerBucket == 4) {
                 return hasvalue12(v1, tag) || hasvalue12(v2, tag);
-            } else if (bits_per_tag == 16 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 16 && slotsPerBucket == 4) {
                 return hasvalue16(v1, tag) || hasvalue16(v2, tag);
             } else {
-                for (size_t j = 0; j < kTagsPerBucket; j++) {
+                for (size_t j = 0; j < slotsPerBucket; j++) {
                     if ((ReadTag(i1, j) == tag) || (ReadTag(i2, j) == tag)) {
                         return true;
                     }
@@ -166,24 +163,24 @@ namespace CuckooHash{
 
         inline bool FindTagInBucket(const size_t i, const uint64_t tag) const {
             // caution: unaligned access & assuming little endian
-            if (bits_per_tag == 4 && kTagsPerBucket == 4) {
+            if (bits_per_slot == 4 && slotsPerBucket == 4) {
                 const char *p = buckets_[i].bits_;
                 uint64_t v = *(uint64_t *)p;  // uint16_t may suffice
                 return hasvalue4(v, tag);
-            } else if (bits_per_tag == 8 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 8 && slotsPerBucket == 4) {
                 const char *p = buckets_[i].bits_;
                 uint64_t v = *(uint64_t *)p;  // uint32_t may suffice
                 return hasvalue8(v, tag);
-            } else if (bits_per_tag == 12 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 12 && slotsPerBucket == 4) {
                 const char *p = buckets_[i].bits_;
                 uint64_t v = *(uint64_t *)p;
                 return hasvalue12(v, tag);
-            } else if (bits_per_tag == 16 && kTagsPerBucket == 4) {
+            } else if (bits_per_slot == 16 && slotsPerBucket == 4) {
                 const char *p = buckets_[i].bits_;
                 uint64_t v = *(uint64_t *)p;
                 return hasvalue16(v, tag);
             } else {
-                for (size_t j = 0; j < kTagsPerBucket; j++) {
+                for (size_t j = 0; j < slotsPerBucket; j++) {
                     if (ReadTag(i, j) == tag) {
                         return true;
                     }
@@ -193,7 +190,7 @@ namespace CuckooHash{
         }
 
         inline bool DeleteTagFromBucket(const size_t i, const uint64_t tag) {
-            for (size_t j = 0; j < kTagsPerBucket; j++) {
+            for (size_t j = 0; j < slotsPerBucket; j++) {
                 if (ReadTag(i, j) == tag) {
                     assert(FindTagInBucket(i, tag) == true);
                     WriteTag(i, j, 0);
@@ -205,14 +202,14 @@ namespace CuckooHash{
 
         inline bool InsertTagToBucket(const size_t i, const uint64_t tag,
                                       const bool kickout, uint64_t &oldtag) {
-            for (size_t j = 0; j < kTagsPerBucket; j++) {
+            for (size_t j = 0; j < slotsPerBucket; j++) {
                 if (ReadTag(i, j) == 0) {
                     WriteTag(i, j, tag);
                     return true;
                 }
             }
             if (kickout) {
-                size_t r = rand() & (kTagsPerBucket - 1);
+                size_t r = rand() & (slotsPerBucket - 1);
                 oldtag = ReadTag(i, r);
                 WriteTag(i, r, tag);
             }
@@ -221,7 +218,7 @@ namespace CuckooHash{
 
         inline size_t NumTagsInBucket(const size_t i) const {
             size_t num = 0;
-            for (size_t j = 0; j < kTagsPerBucket; j++) {
+            for (size_t j = 0; j < slotsPerBucket; j++) {
                 if (ReadTag(i, j) != 0) {
                     num++;
                 }
