1d745fd0880908c6a7d9a4174309e88fe279ec19
grenze <hit.grenze.plus@gmail.com>
2019-02-20 17:28:27 +0800

    tidy

--- timing.h
+++ timing.h
@@ -6,7 +6,13 @@
 #define CUCKOOHASH_TIMING_H
 
 #include <cstdint>
+#include <chrono>
 
-::std::uint64_t NowNanos();
+::std::uint64_t NowNanos() {
+    return static_cast<uint64_t>(::std::chrono::duration_cast<::std::chrono::nanoseconds>(
+                ::std::chrono::steady_clock::now().time_since_epoch())
+                .count());
+}
+
 
 #endif //CUCKOOHASH_TIMING_H
